057913cc2694bb7629ce69768fe46311e26e6fc3
MatveyKanishev <matveykanishev@yandex.ru>
2021-04-18 15:01:46 +0800

    0.00.2 -код: --DatabaseMess.java//создание базы данных

--- app/src/main/java/com/example/resend/DatabaseMess.java
+++ app/src/main/java/com/example/resend/DatabaseMess.java
@@ -3,18 +3,33 @@ package com.example.resend;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Message;
+import android.os.Messenger;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseMess extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "Mess";
+    private static final String DATABASE_NAME = "Mess";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String KEY_GROUP="Group_";//группа(чат)
+    private static final String KEY_MESS="Mess"; //седержание сообщения
+    private static final String KEY_SENDER="Sender";//отправитель
+    private static final String KEY_RECIPIENT = "Recipient";//получатель
+    private static final String KEY_MESSENDER= "Messenger";//соц. сеть.
+    private static final String KEY_TIME ="Time";//время
+    private static final String KEY_ID="Id";// уникальный id сообщения
+
 
     public DatabaseMess(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String mess = "CREATE TABLE " + KEY_ID + "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_MESSENDER + "TEXT, "+ KEY_SENDER + "TEXT, "+ KEY_RECIPIENT + "TEXT, " +
+                 KEY_MESS + "TEXT, "+ KEY_GROUP + "TEXT, "+ KEY_TIME + "NUMERIC); ";
+        sqLiteDatabase.execSQL(mess);
 
     }
 
