513c6d67280234517b3c229fca0e788544a2c5d8
MatveyKanishev <matveykanishev@yandex.ru>
2021-04-18 13:37:59 +0800

    0.00.1 -разметка: --Экран входа

--- app/src/main/java/com/example/resend/DatabaseMess.java
+++ app/src/main/java/com/example/resend/DatabaseMess.java
@@ -1,6 +1,25 @@
 package com.example.resend;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class DatabaseMess extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Mess";
+
+    public DatabaseMess(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
